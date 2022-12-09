4cc7686f1175413f40882d9bc5ee18f3819e842f
Max Pogorelov <colpastores@mail.ru>
2022-12-10 00:02:11 +0300

    crawlWeb/ Create classes that will correspond to the 'site' and 'page' tables in the database.  The index must be set by the 'path' field in the 'page' table.

--- src/main/java/searchengine/model/Page.java
+++ src/main/java/searchengine/model/Page.java
@@ -1,4 +1,32 @@
 package searchengine.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "page")
+@Table(indexes = @Index(name = "path_index", columnList = "path", unique = true))
+@NoArgsConstructor
+@Getter
+@Setter
 public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name= "site_id")
+    Site site;
+
+    @Column(columnDefinition = "varchar(255) NOT NULL")
+    String path;
+
+    @Column(columnDefinition = " INT NOT NULL")
+    int code;
+
+    @Column(columnDefinition = "MEDIUMTEXT NOT NULL")
+    String content;
 }
--- src/main/java/searchengine/model/Site.java
+++ src/main/java/searchengine/model/Site.java
@@ -1,4 +1,35 @@
 package searchengine.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity(name = "site")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Site {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column(columnDefinition = " ENUM('INDEXING', 'INDEXED', 'FAILED') NOT NULL")
+    Status status;
+
+    @Column(name = "status_time", columnDefinition = "DATETIME NOT NULL")
+    LocalDateTime statusTime;
+
+    @Column(name = "last_error", columnDefinition = "TEXT")
+    String lastError;
+
+    @Column(columnDefinition = "VARCHAR(255) NOT NULL")
+    String url;
+
+    @Column(columnDefinition = "VARCHAR(255) NOT NULL")
+    String name;
+
 }
