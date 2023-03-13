ff3000767353d3e25da155ce21382fd2846fdbfb
Max Pogorelov <colpastores@mail.ru>
2023-03-13 21:36:28 +0300

    siteIndexing/ Create a class with a method that will take text as a parameter and return a list of lemmas for each word in this text. Implement a method that will clean the code of web pages from HTML tags. Implement a method that will divide the text into words./

---
+++ src/main/java/searchengine/lemmatizer/Lemmatizer.java
@@ -0,0 +1,72 @@
+package searchengine.lemmatizer;
+
+import org.apache.lucene.morphology.LuceneMorphology;
+import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class Lemmatizer {
+
+    private static final LuceneMorphology luceneMorph;
+
+    private static final String[] partsOfSpeech = new String[]{"ЧАСТ", "МЕЖД", "СОЮЗ", "ПРЕДЛ"};
+
+    static {
+        try {
+            luceneMorph = new RussianLuceneMorphology();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Lemmatizer() {
+    }
+
+    /*TODO:метод, который будет очищать код веб-страниц от HTML-тегов.*/
+    private String cleanupContent(String content) {
+        return Jsoup.clean(content, Safelist.none());
+    }
+
+    private String[] splitTextIntoWords(String text) {
+        String[] words;
+
+        if (text.isEmpty()) {
+            words = new String[1];
+        } else {
+            words = text.replaceAll("([^А-яёЁ])", " ").split("[\\s-!.?;,/:©]+");   //(\d)[^А-яёЁ\w]
+        }
+        return words;
+    }
+
+    public HashMap<String, Integer> getLemmas(String text) {
+        HashMap<String, Integer> lemmas = new HashMap<>();
+        String lemma = "";
+        Integer count = 1;
+        String[] words = splitTextIntoWords(cleanupContent(text).toLowerCase());
+        for (String word : words) {
+            if (!anyNormalFormsIsParticle(luceneMorph.getMorphInfo(word))) {
+                lemma = luceneMorph.getNormalForms(word).get(0);
+                lemmas.merge(lemma, count, (a, b) -> b += 1);
+            }
+        }
+        return lemmas;
+    }
+
+    private boolean anyNormalFormsIsParticle(List<String> wordsMorphInfo) {
+        return wordsMorphInfo.stream().anyMatch(s -> isParticle(s));
+    }
+
+    private boolean isParticle(String morphInfo) {
+        for (String property : partsOfSpeech) {
+            if (morphInfo.contains(property)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
